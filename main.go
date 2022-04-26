0ae0d43541f70e76f9c8f5afecb90e62eff70877
TastenDrueckr <91882377+TastenDrueckr@users.noreply.github.com>
2022-04-26 19:59:25 +0200

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Workshop"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
